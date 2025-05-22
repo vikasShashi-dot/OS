25e6fae18b4dd7604c87aa198555f1c4d664450f
vikasShashi-dot <vikasshashi.cs23@bmsce.ac.in>
2025-05-22 14:47:00 +0530

    Update CIE(22-05-25).c

--- CIE(22-05-25).c
+++ CIE(22-05-25).c
@@ -51,3 +51,54 @@ int main(){
     }
     printf("The hit:%d and Miss:%d", hits, pagef);
 }
+
+
+
+//FIFO
+
+#include<stdio.h>
+
+int main(){
+    int i, j, k, n, frames, hits = 0, pagef = 0,index=0;
+    char pages[100];
+    printf("Enter no. of pages:");
+    scanf("%d", &n);
+    printf("Enter page strings:");
+    scanf("%s", pages);
+    printf("Enter no. of pages:");
+    scanf("%d", &frames);
+    int mem[frames], used[frames];
+    for (i = 0; i < frames;i++)
+    {
+        mem[i] = -1;
+       // used[i] = -1;
+    }
+    for (i = 0; i < n;i++)
+    {
+        int page = pages[i] - '0';
+        int found = 0;
+        for (j = 0; j < frames;j++)
+        {
+            if(mem[j]==page)
+            {
+                hits++;
+                found = 1;
+                //used[j] = i;
+                break;
+            }
+        }
+        if(!found){
+            mem[index] = page;
+            index = (index + 1) % frames;
+            pagef++;
+        }
+        for (j = 0; j < frames;j++)
+        {
+            printf("%d\t", mem[j]);
+        }
+        printf("\n");
+    }
+    printf("The hit:%d and Miss:%d", hits, pagef);
+    //printf("The ratio is:%.2f", (float)(hits / pagef));
+}
+
